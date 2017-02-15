c6828e5dd271449badec073850a11928fa4de564
Carlos Robles <carlos_robles@epam.com>
2017-02-15 10:37:52 -0600

    Finished kata

--- src/app.js
+++ src/app.js
@@ -1,5 +1,33 @@
-import 'babel-polyfill';
+export default function lookAndSay(num, times = 1) {
+  let iteration = 0,
+      output = num.toString();
 
-export default function lookAndSay(start, n){
-  // Do the Magic
+  function tallyNumbers(num){
+    const nums = num.split('');
+    let counters = [[]],
+        position = 0,
+        previous = nums[0];
+
+    nums.forEach((number, index) => {
+      const current = nums[index];
+      if (previous === current) {
+        counters[position].push(current);
+      } else {
+        const next = position + 1;
+        counters.push([]);
+        counters[next].push(current);
+        position++;
+      }
+      previous = current;
+    });
+
+    output = counters.map(count => count.length + count[0]).join('');
+  }
+
+  while (iteration < times){
+    tallyNumbers(output);
+    iteration++;
+  }
+
+  return output;
 }
