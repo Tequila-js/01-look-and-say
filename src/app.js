fe9e420fe7b4dc77d3bd86d48dd52bb381d13a0a
Carlos Perez <cperez@eharmony.com>
2017-02-14 23:23:30 -0600

    Fixed test and refactored solution

--- src/app.js
+++ src/app.js
@@ -1,31 +1,25 @@
 import "babel-polyfill";
 
 function lookAndSay(start, n) {
-    return Array(n).fill(0).reduce(acc => {
-        return look(acc);
-    }, start);
+    return Array(n).fill(0).reduce(acc => look(acc), start);
 }
 
-function look(start){
-    const ary =  start.toString().split("");
+function look(start) {
+    const ary = start.toString().split("");
 
-    const say = ary.reduce((acc, item, index) => {
-       const prev = acc[acc.length-1];
-       var obj = {};
+    const groupByDigit = ary.reduce((acc, item) => {
+       const previousGroup = acc[acc.length-1] || false;
 
-       if(index === 0 || !prev.hasOwnProperty(item)) {
-           obj[item] = 1;
-       }
-       else {
-           obj[item] = prev[item] + 1;
-           return acc.slice(0,-1).concat(obj);
+       if(!previousGroup.hasOwnProperty(item)) {
+           return acc.concat({ [item]: 1 });
        }
 
-       return acc.concat(obj);
-
+       return acc.slice(0,-1).concat({
+           [item]: previousGroup[item] + 1
+       });
     }, []);
 
-    return say.reduce((acc, item) => {
+    return groupByDigit.reduce((acc, item) => {
         const [key, value] = Object.entries(item)[0];
         return acc + value + key;
     }, "");
